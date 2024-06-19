bf887649f0c106b298fb984629d9950b4496de41
Jasur <toshonovj@gmail.com>
2024-06-19 15:02:27 +0500

    Install livewire 3 and node js, create order and product and user resources

---
+++ app/Filament/Resources/ProduktResource/Pages/CreateProdukt.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ProduktResource\Pages;
+
+use App\Filament\Resources\ProductResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateProdukt extends CreateRecord
+{
+    protected static string $resource = ProductResource::class;
+}
---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1 class="text-4xl text-red-500">Home page bro</h1>
+</div>
